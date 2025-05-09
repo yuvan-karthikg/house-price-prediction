c80a026f4dce4a6c82b464e9b05a2623635ab2c4
YUVAN KARTHIK G <yuvankarthikganesan@gmail.com>
2025-05-09 22:15:12 +0400

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -55,7 +55,7 @@ st.header("Model Training")
 def r2_keras(y_true, y_pred):
     SS_res = K.sum(K.square(y_true - y_pred))
     SS_tot = K.sum(K.square(y_true - K.mean(y_true)))
-    return (1 - SS_res/(SS_tot + K.epsilon()))
+    return (1 - SS_res / (SS_tot + K.epsilon()))
 
 # Build improved model with the custom R-squared metric
 def build_improved_model(input_dim):
